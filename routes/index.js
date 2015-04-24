5ad2da5363ada0d7758aa70fdafb821ad7cf4cfc
John Abraham <john@eeaust.com.au>
2015-04-24 14:47:22 +1000

    auth

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,17 @@
 var express = require('express');
 var router = express.Router();
+var auth  = require('../helpers/auth');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
+});
+
+router.post('/login', function(req, res, next) {
+  console.log('req', req);
+  console.log('res', res);
+  auth.login(req, res);
+  //res.json({ message: 'POST courses endpoint works' });
 });
 
 module.exports = router;
